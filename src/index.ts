224ca9215e18ab2b77783407f1f0cb84af1971d7
virgo <virgostevanus27@gmail.com>
2025-10-27 15:12:08 +0700

    Setup Typescript

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// src/index.ts
+function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+function farewell(name: string): string {
+  return `Goodbye, ${name}!`;
+}
+
+console.log(greet("TypeScript"));
+
+console.log(farewell("TypeScript"));
